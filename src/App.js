18220b557e48f22a3a8da942df2970e52ca7ea91
dong young <dong5854@gmail.com>
2022-12-11 00:05:02 +0900

    feat: loading page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import {useEffect, useState} from "react";
+
 function App() {
-  return (
-    <div>
-      <h1>Welcome Back!</h1>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+  const getUser = async () => {
+      await console.log("사용자 정보 받아오는 API");
+      setTimeout(() => {
+          setUser(["유저명", "유저 정보"]);
+          setLoading(false);
+      }, 1000);
+  }
+  useEffect(() => {
+      getUser();
+  }, []);
+  return (<div>
+      {loading ? (
+          <h1>Loading...</h1>
+      ) : (
+          <div>
+              {user.map((userInfo, index) => (
+                      <div key={index}>{userInfo}</div>
+              ))}
+          </div>
+      )
+      }</div>);
 }
 
 export default App;
